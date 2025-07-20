103e8c926e7a9b357049da9d4c7b92b3ac0bbea1
Shahdd21 <88803131+Shahdd21@users.noreply.github.com>
2025-07-21 02:02:37 +0300

    added the url of the user service

--- src/main/java/com/example/ProductService/feign/UserClient.java
+++ src/main/java/com/example/ProductService/feign/UserClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "UserAuthenticationService")
+@FeignClient(name = "UserAuthenticationService", url = "${auth.service.url}")
 public interface UserClient {
 
     @GetMapping("/users/check-role")
